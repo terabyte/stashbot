d0462fe8a55add016a1bf7a86f9d463bf5927966
Carl Myers <cmyers@cloudera.com>
2016-10-04 15:05:20 -0700

    Fix DOMAIN_VERSION to be long, even when building on a tag

--- bin/invoke-sdk.sh
+++ bin/invoke-sdk.sh
@@ -4,7 +4,7 @@
 bin/install-plugin-sdk-linux.sh
 
 if [[ "x$DOMAIN_VERSION" == "x" ]]; then
-    export DOMAIN_VERSION=`git describe --dirty="-dirty" --abbrev=12`
+    export DOMAIN_VERSION=`git describe --dirty="-dirty" --abbrev=12 --long`
 fi
 
 .sdk/bin/atlas-mvn "$@"
